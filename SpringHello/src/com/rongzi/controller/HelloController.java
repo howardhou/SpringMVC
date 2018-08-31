6c7f727e5032a6938e3aff0a9a1847e96a13d2c3
houdongdong <houdongdong@rongzi.com>
2018-08-31 10:43:26 +0800

    第一个Spring MVC 应用 - 基于Controller 接口的控制器和基于注解的控制器

--- SpringHello/src/com/rongzi/controller/HelloController.java
+++ SpringHello/src/com/rongzi/controller/HelloController.java
@@ -5,12 +5,15 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class HelloController implements Controller {
 
     private static final Log logger = LogFactory.getLog(HelloController.class);
 
     @Override
-    public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         logger.info("handleRequest 被调用");
 
         ModelAndView mv = new ModelAndView();
