83ff95df9202a735e4e55432ed53910bc48449bb
houdongdong <houdongdong@rongzi.com>
2018-09-18 14:55:58 +0800

    使用 Bean 的作用域 - request

--- SpringHello/src/com/rongzi/controller/User3Controller.java
+++ SpringHello/src/com/rongzi/controller/User3Controller.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/user3")
@@ -16,10 +20,18 @@ public class User3Controller {
 
     // http://localhost:8080/user3/info
     @RequestMapping(value = "/info", method = RequestMethod.GET)
-    public String userInfo(Model model){
+    public String userInfo(Model model, HttpServletRequest request){
         logger.info("GET userInfo");
 
-        User user = new User();
+        // 每次请求都一样
+        System.out.println(this);
+
+        // 获取 Spring容器 - ApplicationContext
+        WebApplicationContext applicationContext = (WebApplicationContext) request.getAttribute(DispatcherServlet.WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+        User user = applicationContext.getBean("user", User.class);
+        System.out.println(user);
+//        User user = new User();
+
         user.setUsername("侯冬冬");
         user.setSex("男");
         user.setAge(36);
